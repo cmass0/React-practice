396b705c7e6a43ee45de1564821a5842b6d6d6ab
cmass0 <mnz1223@naver.com>
2023-05-30 21:28:13 +0900

    re

--- src/components/Modal/ErrorModal.js
+++ src/components/Modal/ErrorModal.js
@@ -1,41 +1,55 @@
 import React from "react";
 import "./ErrorModal.css";
-import ReactDom from "react-dom";
+// import ReactDom from "react-dom";
 
-const Backdrop = (props) => {
-  return <div className="modal-effect" onClick={props.onReset} />;
-};
+// const Backdrop = (props) => {
+//   return <div className="modal-effect" onClick={props.onReset} />;
+// };
 
-const OverlayModal = (props) => {
-  return (
-    <form id="modal">
-      <div className="top">{props.title}</div>
-      <div className="body">
-        {props.message}
-        <div>
-          <button className="modal-btn" onClick={props.onReset}>
-            Okay
-          </button>
-        </div>
-      </div>
-    </form>
-  );
-};
+// const OverlayModal = (props) => {
+//   return (
+// <form id="modal">
+//   <div className="top">{props.title}</div>
+//   <div className="body">
+//     {props.message}
+//     <div>
+//       <button className="modal-btn" onClick={props.onReset}>
+//         Okay
+//       </button>
+//     </div>
+//   </div>
+// </form>
+//   );
+// };
 function ErrorModal(props) {
   return (
+    // <React.Fragment>
+    //   {ReactDom.createPortal(
+    //     <Backdrop onClick={props.onReset} />,
+    //     document.getElementById("backdrop-root")
+    //   )}
+    //   {ReactDom.createPortal(
+    //     <OverlayModal
+    //       title={props.title}
+    //       message={props.message}
+    //       onClick={props.onReset}
+    //     />,
+    //     document.getElementById("errormodal-root")
+    //   )}
+    // </React.Fragment>
     <React.Fragment>
-      {ReactDom.createPortal(
-        <Backdrop onClick={props.onReset} />,
-        document.getElementById("backdrop-root")
-      )}
-      {ReactDom.createPortal(
-        <OverlayModal
-          title={props.title}
-          message={props.message}
-          onClick={props.onReset}
-        />,
-        document.getElementById("errormodal-root")
-      )}
+      <div className="modal-effect" onClick={props.onReset} />
+      <form id="modal">
+        <div className="top">{props.title}</div>
+        <div className="body">
+          {props.message}
+          <div>
+            <button className="modal-btn" onClick={props.onReset}>
+              Okay
+            </button>
+          </div>
+        </div>
+      </form>
     </React.Fragment>
   );
 }
